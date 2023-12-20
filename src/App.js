eda8b5f508bb17897b142850a45bfc12e6a5a3bd
Macbook <macbook@MacBook-Pro-cua-Sinhdz.local>
2023-12-20 15:42:32 +0700

    menu side bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { Space, Typography } from "antd";
+import {
+  DashboardOutlined,
+  HomeFilled,
+  RedEnvelopeOutlined,
+} from "@ant-design/icons";
+import { Input, Menu, Space, Typography } from "antd";
 const { Text, Link, Paragraph } = Typography;
 function App() {
   return (
@@ -13,27 +18,49 @@ function App() {
         }}
       >
         <Space direction="vertical">
-          <Text>Ant Design (default)</Text>
-          <Text type="secondary">Ant Design (secondary)</Text>
-          <Text type="success">Ant Design (success)</Text>
-          <Text type="warning">Ant Design (warning)</Text>
-          <Text type="danger">Ant Design (danger)</Text>
-          <Text disabled>Ant Design (disabled)</Text>
-          <Text mark>Ant Design (mark)</Text>
-          <Text code>Ant Design (code)</Text>
-          <Text keyboard>Ant Design (keyboard)</Text>
-          <Text underline>Ant Design (underline)</Text>
-          <Text delete>Ant Design (delete)</Text>
-          <Text strong>Ant Design (strong)</Text>
-          <Text italic>Ant Design (italic)</Text>
-          <Paragraph
-            copyable={{ text: "dsfsdf", tooltips: ["Click to coppy", "Done"] }}
-          >
-            Ant Design (italic)
-          </Paragraph>
-          <Link href="https://ant.design" target="_blank">
-            Ant Design (Link)
-          </Link>
+          <Menu
+            mode="inline"
+            defaultOpenKeys={["dashboard"]}
+            items={[
+              {
+                label: (
+                  <Input.Search placeholder="Search here..."></Input.Search>
+                ),
+                key: "search",
+              },
+              {
+                label: "Home",
+                key: "home",
+                icon: <HomeFilled />,
+              },
+              {
+                label: "Dashboard",
+                key: "dashboard",
+                icon: <DashboardOutlined />,
+                children: [
+                  {
+                    label: "Revenue",
+                    key: "rev",
+                    icon: <RedEnvelopeOutlined />,
+                  },
+                  { label: "Expenses", key: "exp" },
+                ],
+              },
+              {
+                label: "User Management",
+                key: "um",
+                children: [
+                  { label: "Edit Profile", key: "ep" },
+                  { label: "Switch Account", key: "sa" },
+                ],
+              },
+              {
+                label: "Signout",
+                key: "signout",
+                danger: true,
+              },
+            ]}
+          ></Menu>
         </Space>
       </header>
     </div>
