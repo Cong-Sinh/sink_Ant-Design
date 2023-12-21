8a34299af8d350e23f2f3dc415ef86695a5221b9
Macbook <macbook@MacBook-Pro-cua-Sinhdz.local>
2023-12-21 15:03:20 +0700

    dropdown click

--- src/App.js
+++ src/App.js
@@ -1,115 +1,79 @@
-import { HomeOutlined, PoweroffOutlined } from "@ant-design/icons";
-import { Menu, Space } from "antd";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import {
+  CopyOutlined,
+  DeleteOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
+  ReloadOutlined,
+} from "@ant-design/icons";
+import { Dropdown, Menu } from "antd";
 function App() {
+  const menu = (
+    <Menu
+      onClick={({ key }) => {
+        console.log("you click on ", key);
+      }}
+      items={[
+        {
+          label: "Coppy",
+          key: "coppy",
+          icon: <CopyOutlined />,
+        },
+        {
+          label: "Share",
+          key: "share",
+          children: [
+            {
+              label: "Facebook",
+              key: "facebook",
+              icon: <FacebookOutlined />,
+            },
+            {
+              label: "Instagram",
+              key: "instagram",
+              icon: <InstagramOutlined />,
+            },
+          ],
+        },
+        {
+          label: "Reload",
+          key: "reload",
+          icon: <ReloadOutlined />,
+        },
+        {
+          label: "Delete",
+          key: "delete",
+          icon: <DeleteOutlined />,
+        },
+      ]}
+    ></Menu>
+  );
   return (
     <div className="App">
       <header
         className="App-header"
-        style={
-          {
-            // display: "flex",
-            // justifyContent: "center",
-            // alignItems: "center",
-            // height: "100vh",
-          }
-        }
-      >
-        <div>
-          <Header />
-          <div style={{ display: "flex" }}>
-            <SideMenu />
-            <Content />
-          </div>
-          <Footer />
-        </div>
-      </header>
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div
-      style={{
-        backgroundColor: "blue",
-        width: "100%",
-        height: "100px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "24px",
-        color: "white",
-      }}
-    >
-      Header
-    </div>
-  );
-}
-
-function SideMenu() {
-  const navigate = useNavigate();
-
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <Menu
-        onClick={({ key }) => {
-          if (key === "signout") {
-          } else {
-            navigate(key);
-          }
+        style={{
+          // display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
         }}
-        items={[
-          { label: "Home", key: "/", icon: <HomeOutlined /> },
-          { label: "Dashboard", key: "/dashboard" },
-          {
-            label: "Users List",
-            key: "/usersList",
-            children: [
-              { label: "Signout", key: "/signout" },
-              { label: "signup", key: "/signup" },
-            ],
-          },
-          { label: "Profile", key: "/profile" },
-          {
-            label: "Signout",
-            key: "/signout",
-            icon: <PoweroffOutlined />,
-          },
-        ]}
-      ></Menu>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div
-      style={{
-        backgroundColor: "green",
-        width: "100%",
-        height: "100px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "24px",
-        color: "white",
-      }}
-    >
-      Footer
-    </div>
-  );
-}
-
-function Content() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<div>Home</div>}></Route>
-        <Route path="/Dashboard" element={<div>Dashboard</div>}></Route>
-        <Route path="/UsersList" element={<div>UsersList</div>}></Route>
-        <Route path="/Profile" element={<div>Profile dsfsdf</div>}></Route>
-      </Routes>
+      >
+        <Dropdown overlay={menu} trigger={["contextMenu"]}>
+          <div
+            style={{
+              width: "200px",
+              height: "200px",
+              backgroundColor: "lightgray",
+              border: "1px solid red",
+            }}
+          >
+            Click Me
+          </div>
+        </Dropdown>
+        <Dropdown overlay={menu} trigger={["contextMenu"]}>
+          <div>Click Me</div>
+        </Dropdown>
+      </header>
     </div>
   );
 }
